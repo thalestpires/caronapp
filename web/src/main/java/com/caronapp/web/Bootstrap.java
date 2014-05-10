6382cb95f486bccc161759fbd20e1beb608eade9
Thales Pires <thalestpires@gmail.com>
2014-05-10 15:03:05 -0300

    Bootstrap

--- web/src/main/java/com/caronapp/web/Bootstrap.java
+++ web/src/main/java/com/caronapp/web/Bootstrap.java
@@ -21,39 +21,6 @@ public class Bootstrap extends HttpServlet {
 	@Override
 	public void init(ServletConfig config) throws ServletException {
 		
-		DBCollection collection = DBUtil.getInstance().getDatabase().getCollection(DBUtil.COLLECTION_CARONAS);
-		
-		collection.drop();
-		
-		Carona carona1 = new Carona(1,"100000075065275",
-									"Luiz Felipe" ,"Barra","Fundão", 
-									Calendar.getInstance().getTime());
-		collection.insert(RetornaQuery(carona1));
-		
-		Carona carona2 = new Carona(2,"100000075065275",
-				"Luiz Felipe" ,"Barra","Barra Music", 
-				Calendar.getInstance().getTime());
-		
-		collection.insert(RetornaQuery(carona2));
-		
-		Carona carona3 = new Carona(3,"100000075065275",
-				"Luiz Felipe" ,"Via Parque","Botafogo", 
-				Calendar.getInstance().getTime());
-		
-		collection.insert(RetornaQuery(carona3));
-	}
-
-	private BasicDBObject RetornaQuery(Carona carona) {
-		BasicDBObject query = new BasicDBObject();
-
-		query.put("_id" ,carona.getId());
-		query.put("motoristaFacebookId" ,carona.getMotoristaFacebookId());
-		query.put("nome" ,carona.getNome());
-		query.put("origem" ,carona.getOrigem());
-		query.put("destino" ,carona.getDestino());
-		query.put("data" ,carona.getData());
-		
-		return query;
 	}
 
 	@Override
