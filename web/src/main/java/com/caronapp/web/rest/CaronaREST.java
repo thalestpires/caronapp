2c410c9ac782e22f8776af4a58a94e99a51e3184
Thales Pires <thalestpires@gmail.com>
2014-05-10 14:58:03 -0300

    Insercao no Banco ( Back end)

--- web/src/main/java/com/caronapp/web/rest/CaronaREST.java
+++ web/src/main/java/com/caronapp/web/rest/CaronaREST.java
@@ -1,8 +1,7 @@
 package com.caronapp.web.rest;
 
-import java.util.List;
-
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -10,11 +9,12 @@ import javax.ws.rs.Produces;
 import org.apache.log4j.Logger;
 import org.json.JSONException;
 
-import com.caronapp.model.Carona;
 import com.caronapp.web.util.DBUtil;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.util.JSON;
 
 @Path("/carona")
 public class CaronaREST extends AbstractREST {
@@ -30,9 +30,19 @@ public class CaronaREST extends AbstractREST {
 		return prettySerialize(cursor);
 	}
 	
+	@POST
+	@Produces(JSON_UTF8)
+	public void guardaCarona(String carona) throws JSONException {
+		DBCollection collection = DBUtil.getInstance().getDatabase().getCollection(DBUtil.COLLECTION_CARONAS);
+		
+		DBObject dbObject = (DBObject) JSON.parse(carona);
+		
+		collection.insert(dbObject);
+	}
+	
 	//TODO fazer query de caronas
 	@GET
-	@Path("{userID}")
+	@Path("{caronaID}")
 	@Produces(JSON_UTF8)
 	public String findCaronas(@PathParam("userID") Long userID) {
 		DBCollection collection = DBUtil.getInstance().getDatabase().getCollection(DBUtil.COLLECTION_CARONAS);
@@ -41,4 +51,17 @@ public class CaronaREST extends AbstractREST {
 		DBCursor cursor = collection.find(query);
 		return serialize(cursor);
 	}
+	
+	@GET
+	@Path("/minha/{meuID}")
+	@Produces(JSON_UTF8)
+	public String findMinhasCaronas(@PathParam("meuID") Long userID) {
+		DBCollection collection = DBUtil.getInstance().getDatabase().getCollection(DBUtil.COLLECTION_CARONAS);
+		BasicDBObject query = new BasicDBObject();
+		//TODO fazer query de caronas
+		DBCursor cursor = collection.find(query);
+		return serialize(cursor);
+	}
+	
+
 }
