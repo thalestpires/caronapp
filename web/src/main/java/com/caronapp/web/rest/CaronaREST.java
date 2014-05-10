56e6d2ca6d2d23ff133a926275c85697e779e238
Thales Pires <thalestpires@gmail.com>
2014-05-10 11:55:40 -0300

    WebService retornando lista de caronas a partir do BD, no formato Json

--- web/src/main/java/com/caronapp/web/rest/CaronaREST.java
+++ web/src/main/java/com/caronapp/web/rest/CaronaREST.java
@@ -1,12 +1,16 @@
 package com.caronapp.web.rest;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import org.apache.log4j.Logger;
+import org.json.JSONException;
 
+import com.caronapp.model.Carona;
 import com.caronapp.web.util.DBUtil;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
@@ -17,6 +21,16 @@ public class CaronaREST extends AbstractREST {
 
 	Logger logger = Logger.getLogger(CaronaREST.class);
 	
+	@GET
+	@Produces(JSON_UTF8)
+	public String findCaronas() throws JSONException {
+		DBCollection collection = DBUtil.getInstance().getDatabase().getCollection(DBUtil.COLLECTION_CARONAS);
+		BasicDBObject query = new BasicDBObject();
+		DBCursor cursor = collection.find(query);
+		return prettySerialize(cursor);
+	}
+	
+	//TODO fazer query de caronas
 	@GET
 	@Path("{userID}")
 	@Produces(JSON_UTF8)
