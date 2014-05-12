023c54d98d85daf9fbf6599e2dff0e82350c7a0d
jlnery <jlnery@gmail.com>
2014-05-12 01:11:04 -0300

    Transição da lista de "Minhas Caronas" para "Adicionar Carona"

--- carona_android-parent/carona_android/src/main/java/com/caronapp/android/MinhasCaronasListActivity.java
+++ carona_android-parent/carona_android/src/main/java/com/caronapp/android/MinhasCaronasListActivity.java
@@ -2,50 +2,69 @@ package com.caronapp.android;
 
 import java.util.List;
 
-import com.caronapp.model.Carona;
-
 import android.app.ListActivity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
-import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.Toast;
+
+import com.caronapp.model.Carona;
 
 public class MinhasCaronasListActivity extends ListActivity {
 
-    TextView content;
-    
-    List<Carona> caronas;
-    
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	TextView content;
 
-        new FetchMinhasCaronasTask(this).execute();
-   }
-	
-	public void updateCaronas(List<Carona> caronas){
-		this.caronas = caronas;
-        
-        CaronaAdapterListView adapter = new CaronaAdapterListView(this, caronas);
-        setListAdapter(adapter); 
-        
-        this.getListView().setLongClickable(true);
-        this.getListView().setOnItemLongClickListener(new OnItemLongClickListener() {
-            public boolean onItemLongClick(AdapterView<?> parent, View v, int position, long id) {
+	List<Carona> caronas;
 
-        		startActivity(new Intent(Intent.ACTION_VIEW, 
-        								Uri.parse("fb://messaging/" + getCaronas().get(position).getMotoristaFacebookId() )));
-                
-                return true;
-            }
-        });
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_minhas_caronas);
+
+		new FetchMinhasCaronasTask(this).execute();
 	}
-	
-	public List<Carona> getCaronas(){
+
+	public void updateCaronas(List<Carona> caronas) {
+		this.caronas = caronas;
+
+		CaronaAdapterListView adapter = new CaronaAdapterListView(this, caronas);
+		setListAdapter(adapter);
+
+		this.getListView().setLongClickable(true);
+
+	}
+
+	public List<Carona> getCaronas() {
 		return caronas;
 	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override
+	  public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	    // action with ID action_refresh was selected
+	    case R.id.action_refresh:
+	    	Intent intent = new Intent(getApplicationContext(), CadastroCaronaActivity.class);
+	        startActivity(intent);
+	      break;
+	    // action with ID action_settings was selected
+	    case R.id.action_settings:
+	      Toast.makeText(this, "Settings selected", Toast.LENGTH_SHORT)
+	          .show();
+	      break;
+	    default:
+	      break;
+	    }
+
+	    return true;
+	  } 
 }
