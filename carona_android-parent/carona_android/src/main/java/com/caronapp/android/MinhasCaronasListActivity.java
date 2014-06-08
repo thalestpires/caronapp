fdae2be3d440e327ded816df34f1174a8f8f9af2
rebecabordini <bordini.rebeca@gmail.com>
2014-06-08 20:15:21 -0300

    Layout tela inicial

--- carona_android-parent/carona_android/src/main/java/com/caronapp/android/MinhasCaronasListActivity.java
+++ carona_android-parent/carona_android/src/main/java/com/caronapp/android/MinhasCaronasListActivity.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
